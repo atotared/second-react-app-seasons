0fff87e70314283ffea44bf4ec6eab897449e4c2
atotared <atota@red.software.systems>
2021-02-08 23:03:49 +0100

    Update readme and and lifecycle methods to component

--- README2.md
+++ README2.md
@@ -11,3 +11,20 @@
 3. Updating STATE on a component causes the component to (almost) instantly re-render
 4. STATE must be initialized when a component is created
 5. # STATE can only be updated using the function setState
+
+# What's super keyword?
+
+super is a reference to the parent's (React.Component) constructor function.
+It allows to create a new component that inherits the constructor function from its parent.
+
+# What's a component lifecycle method?
+
+It's a function that we can optionally define inside our class based component. If we decide to implement these methods, they'll be called automatically by React at certain points during a component lifecycle.
+
+# STEPS OF COMPONENT LIFECYCLE
+
+1. _constructor_
+2. _render_ => only one being mandatory, not optional like others - gets called after each component udpate
+3. _componentDidMount_ => content visible on screen - called only once, when component is first rendered onto the screen
+4. _componentDidUpdate_ => sit and wait for updates - updates come from setState
+5. _componentWillUnmount_ => sit and wait until the component is no longer shown
--- src/index.js
+++ src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props); // super is a reference to the parent's (React.Component) constructor function
+    // console.log(this.state); => OUTPUT: undefined
+    this.state = { lat: null, errorMessage: "" }; // This is the "state" object: it's a JS object with a property of "lat", assigned to this.state
+    // console.log(this.state); => OUTPUT: {lat: null}
+
     window.navigator.geolocation.getCurrentPosition(
       // Geolocation API that accepts 2 callback functions as arguments
-      (position) => console.log(position), // Success callback
-      (err) => console.log(err) // Reject callback
+      (position) => this.setState({ lat: position.coords.latitude }), // Success callback - always call setState() to update "state"
+      (err) => this.setState({ errorMessage: err.message }) // Reject callback
     );
-    return <div>Latitude: </div>;
+  }
+
+  componentDidMount() {
+    console.log("Component was first rendered!");
+  }
+
+  componentDidUpdate() {
+    console.log("Component was udpated!");
+  }
+
+  render() {
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    } else if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else {
+      return <div>Loading!</div>;
+    }
   }
 }
 
