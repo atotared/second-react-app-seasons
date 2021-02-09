9d3d5afc7e43831fe56f8d69bcea2cd6e7c8ae0b
atotared <atota@red.software.systems>
2021-02-09 22:46:47 +0100

    Refactor constructor of App component, pass App lat state as props to child component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const SeasonDisplay = () => {
+const SeasonDisplay = (props) => {
+  console.log(props.lat); // Receives state of parent (App component) in index.js as props
   return <div>Season Display</div>;
 };
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
+  /* constructor(props) {
     super(props); // super is a reference to the parent's (React.Component) constructor function
     // console.log(this.state); => OUTPUT: undefined
     this.state = { lat: null, errorMessage: "" }; // This is the "state" object: it's a JS object with a property of "lat", assigned to this.state
     // console.log(this.state); => OUTPUT: {lat: null}
+  } */
+  // Thanks to Babel, the above constructor is equivalent to:
+  state = { lat: null };
 
+  // React's best practices say it's preferrable to place data loading code inside componentDidMount()
+  // instead of the constructor (data loading that we want to perform just once)
+  componentDidMount() {
+    console.log("Component was first rendered!");
     window.navigator.geolocation.getCurrentPosition(
       // Geolocation API that accepts 2 callback functions as arguments
       (position) => this.setState({ lat: position.coords.latitude }), // Success callback - always call setState() to update "state"
@@ -16,17 +23,19 @@ class App extends React.Component {
     );
   }
 
-  componentDidMount() {
-    console.log("Component was first rendered!");
-  }
-
+  // React's best practices say it's preferrable to place data loading code inside componentDidMount()
+  // instead of the constructor (data loading that we want to perform on each component's update)
   componentDidUpdate() {
     console.log("Component was udpated!");
   }
 
+  componentWillUnmount() {
+    console.log("This is used for cleanup.");
+  }
+
   render() {
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat}></SeasonDisplay>; // Take state of parent component and pass it as a prop to a child
     } else if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     } else {
