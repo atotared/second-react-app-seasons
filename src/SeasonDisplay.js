37afe19fd9369aca7829713b92eb4e574fe872f4
atotared <atota@red.software.systems>
2021-02-10 22:57:07 +0100

    Add condition to check season, add season icons, implement SeasonDisplay component's JSX

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,36 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    seasonText: "Let's hit the beach!",
+    seasonIcon: "sun",
+  },
+  winter: {
+    seasonText: "Burr, it's chilly!",
+    seasonIcon: "snowflake",
+  },
+};
+
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? "summer" : "winter";
+  } else {
+    return latitude > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = (props) => {
   console.log(props.lat); // Receives state of parent (App component) in index.js as props
-  return <div>Season Display</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { seasonText, seasonIcon } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={`${seasonIcon} icon`} />
+      <h1>{seasonText}</h1>
+      <i className={`${seasonIcon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
